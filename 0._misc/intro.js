20a221a33a475bb6e6ddce827c5eeeaeac1d1318
Antonio-Banderas <anto8214@stud.kea.dk>
2022-08-29 09:54:26 +0200

    first push

---
+++ 0._misc/intro.js
@@ -0,0 +1,11 @@
+// type coercion/conversion lecture
+const total = 3.32;
+const price = 12;
+
+const sum = total + price;
+
+console.log(total, price); // brug komma til at få printet flere variablier - ikke plus som i Java! (god stil)
+
+const balanceEquals = sum === total
+console.log(balanceEquals); // husk at bruge 3 '=', for strict equality-check (!== er også ok)
+
