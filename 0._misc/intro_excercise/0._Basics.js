b8623c6485fe4df8fb799175701e842c3ae7fc78
Antonio-Banderas <anto8214@stud.kea.dk>
2022-08-29 10:30:15 +0200

    Finished basics exercise

---
+++ 0._misc/intro_excercise/0._Basics.js
@@ -0,0 +1,32 @@
+// --------------------------------------
+// Variables, strings, numbers
+// --------------------------------------
+// Exercise 1 - Console and constiables
+
+const firstName = "Anders";
+const lastName = "Latif";
+
+
+// Print out the following in the console:
+// My first name is Anders and my last name is Latif
+
+console.log("My first name is", firstName, "and my last name is", lastName);
+
+// --------------------------------------
+// Exercise 2 - Numbers and Strings
+
+const year = "2021";
+const number = 1;
+
+// Add the year plus the number
+// The result should be 2022
+// You cannot touch line 1 or 2
+
+const sum0 = parseInt(year) + number
+const sum1 = parseFloat(year) + number
+const sum = Number(year) + number
+console.log(sum0);
+console.log(sum1);
+console.log(sum2);
+
+// --------------------------------------
