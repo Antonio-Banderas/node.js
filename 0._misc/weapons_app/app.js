15ce7d4e6f3bc080939fb3f007c7cb2948aa149b
Antonio-Banderas <anto8214@stud.kea.dk>
2022-09-11 21:04:20 +0200

    polished_pesudo-database_and_objects

--- 0._misc/weapons_app/app.js
+++ 0._misc/weapons_app/app.js
@@ -1,13 +1,19 @@
 const app = require("express")(); 
 
-const weapons = ["rifle","pistol","tank","mecha"]
+// pretend database
+const weapons_database = [
+    { id: 1, type: "rifle" },
+    { id: 2, type: "tank" },
+    { id: 3, type: "mecha" },
+]
 
 app.get("/weapons", (req, res) => {
-    res.send( {weapons:weapons});
+    res.send({weapons:weapons_database});
 });
 
 app.get("/weapons/:id", (req, res) => {
-    res.send(weapons[req.params.id])
+    const weapon = weapons_database.find(weapon => weapon.id === Number(req.params.id))
+    res.send(weapon)
 });
 
 app.listen(8080, () => {console.log("Server is runnning on port",8080);});
