c2ea12fcac118d2d9aa928793e4a6ded4d0963fa
Antonio-Banderas <anto8214@stud.kea.dk>
2022-09-11 20:07:46 +0200

    express

--- 1._Basic_Express/app.js
+++ 1._Basic_Express/app.js
@@ -1,3 +1,4 @@
+
 // const express = require("express"); // importering af bibliotek
 //const app = express(); // instantiering af vores bibliotek
 const app = require("express")(); // inline instantiering
@@ -11,6 +12,23 @@ app.get("/deers", (req, res) => {
     res.send({ size: "BIG"});
 });
 
+app.get("/deers/:id", (req, res) => {
+    if (Number(req.params.id) === "1") { // aldrig brug 2 ==. brug 3 ===.
+        res.send({message:"thats bambi"});
+    } else {
+        res.send({message:"this is someone else"})
+    }
+});
+
+app.get("/actors", (req,res) => {
+    res.send({
+        message:"information about the actor",
+        //info: req.query
+        ...req.query
+    });
+})
+
+
 app.get("/cups", (req, res) => {
     res.send({ 
         colour: "red",
@@ -22,5 +40,5 @@ app.get("/cups", (req, res) => {
 // app.listen(8080) 
 // callback function to validate the server is running
 app.listen(8080, () => {
-    console.log("Server is runnning!");
+    console.log("Server is runnning on port",8080);
 });
